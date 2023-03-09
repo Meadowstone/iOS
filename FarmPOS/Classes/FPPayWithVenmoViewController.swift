21846b0a3937611dc0908cb3575150ad71041048
Denis Mendica <denis.mendica@toptal.com>
2023-03-09 16:22:10 +0100

    Add skeleton for Pay with Venmo screen

---
+++ FarmPOS/Classes/FPPayWithVenmoViewController.swift
@@ -0,0 +1,17 @@
+//
+//  FPPayWithVenmoViewController.swift
+//  Farm POS
+//
+//  Created by Denis Mendica on 09.03.2023..
+//  Copyright © 2023 Eugene Reshetov. All rights reserved.
+//
+
+import UIKit
+
+class FPPayWithVenmoViewController: UIViewController {
+    
+    override func loadView() {
+        view = UIView()
+        view.backgroundColor = .red
+    }
+}
