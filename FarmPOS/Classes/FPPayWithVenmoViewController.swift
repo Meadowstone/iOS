41263bcbeb11a8e101d09d0ad038a0c3e3aad31b
Denis Mendica <denis.mendica@toptal.com>
2023-03-10 14:01:13 +0100

    Add Venmo QR code and Payment submitted button

--- FarmPOS/Classes/FPPayWithVenmoViewController.swift
+++ FarmPOS/Classes/FPPayWithVenmoViewController.swift
@@ -10,8 +10,42 @@ import UIKit
 
 class FPPayWithVenmoViewController: UIViewController {
     
+    private var qrCodeImageView: UIImageView!
+    private var paymentSubmittedButton: UIButton!
+    
     override func loadView() {
         view = UIView()
-        view.backgroundColor = .red
+        view.backgroundColor = FPColorPaymentFlowBackground
+        
+        createQRCodeImageView()
+        createPaymentSubmittedButton()
+    }
+    
+    private func createQRCodeImageView() {
+        qrCodeImageView = UIImageView(image: .init(named: "venmo_qr_code"))
+        
+        view.addSubview(qrCodeImageView)
+        qrCodeImageView.translatesAutoresizingMaskIntoConstraints = false
+        qrCodeImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        qrCodeImageView.topAnchor.constraint(equalTo: view.topAnchor, constant: 16).isActive = true
+    }
+    
+    private func createPaymentSubmittedButton() {
+        paymentSubmittedButton = UIButton()
+        paymentSubmittedButton.setBackgroundImage(UIImage(named: "green_btn"), for: .normal)
+        paymentSubmittedButton.setTitle("Payment submitted", for: .normal)
+        paymentSubmittedButton.titleLabel?.font = UIFont(name: "HelveticaNeue", size: 20)
+        paymentSubmittedButton.addTarget(self, action: #selector(paymentSubmittedTapped), for: .touchUpInside)
+        
+        view.addSubview(paymentSubmittedButton)
+        paymentSubmittedButton.translatesAutoresizingMaskIntoConstraints = false
+        paymentSubmittedButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20).isActive = true
+        paymentSubmittedButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20).isActive = true
+        paymentSubmittedButton.topAnchor.constraint(equalTo: qrCodeImageView.bottomAnchor, constant: 20).isActive = true
+        paymentSubmittedButton.heightAnchor.constraint(equalToConstant: 40).isActive = true
+    }
+    
+    @objc private func paymentSubmittedTapped() {
+        
     }
 }
